9b5db0f5cb51cfc79f11edcd53338fe88e2c8bde
Jianan <qinxiandiqi@foxmail.com>
2024-03-13 01:03:22 +0800

    feature：增加添加按钮

--- app/src/main/java/cn/qinxiandiqi/photochecker/MainActivity.kt
+++ app/src/main/java/cn/qinxiandiqi/photochecker/MainActivity.kt
@@ -3,13 +3,29 @@ package cn.qinxiandiqi.photochecker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cn.qinxiandiqi.photochecker.ui.theme.PhotoCheckerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +34,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             PhotoCheckerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                ImageSelectorScreen {
+
                 }
             }
         }
@@ -30,17 +43,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ImageSelectorScreen(onAddImageClick: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable(onClick = onAddImageClick)
+                    .drawBehind {
+                        val borderStrokeWidth = 1.dp.toPx()
+                        val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                        val cornerRadius = 8.dp.toPx()
+                        // Draw the dashed border
+                        drawRoundRect(
+                            color = Color.Black,
+                            style = Stroke(width = borderStrokeWidth, pathEffect = pathEffect),
+                            cornerRadius = CornerRadius(cornerRadius)
+                        )
+                    },
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .size(100.dp, 180.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Image",
+                        tint = Color.Black,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PhotoCheckerTheme {
-        Greeting("Android")
+    ImageSelectorScreen {
+
     }
 }
