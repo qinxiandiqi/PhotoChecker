ba6be444c9b36bb5179aa5a586e2514f5c623018
Jianan <qinxiandiqi@foxmail.com>
2024-03-14 00:52:08 +0800

    feature:增加图片选择页面

--- app/src/main/java/cn/qinxiandiqi/photochecker/MainActivity.kt
+++ app/src/main/java/cn/qinxiandiqi/photochecker/MainActivity.kt
@@ -3,7 +3,6 @@ package cn.qinxiandiqi.photochecker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,7 +14,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,6 +29,7 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cn.qinxiandiqi.photochecker.ui.theme.PhotoCheckerTheme
@@ -42,41 +48,60 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ImageSelectorScreen(onAddImageClick: () -> Unit) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    Column {
+        TopAppBar(
+            title = { Text(text = stringResource(id = R.string.app_name)) },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            Card(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .clickable(onClick = onAddImageClick)
-                    .drawBehind {
-                        val borderStrokeWidth = 1.dp.toPx()
-                        val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
-                        val cornerRadius = 8.dp.toPx()
-                        // Draw the dashed border
-                        drawRoundRect(
-                            color = Color.Black,
-                            style = Stroke(width = borderStrokeWidth, pathEffect = pathEffect),
-                            cornerRadius = CornerRadius(cornerRadius)
-                        )
-                    },
-                shape = RoundedCornerShape(8.dp)
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Box(
+                Card(
                     modifier = Modifier
-                        .padding(24.dp)
-                        .size(100.dp, 180.dp)
+                        .padding(16.dp)
+                        .clickable(onClick = onAddImageClick)
+                        .drawBehind {
+                            val borderStrokeWidth = 1.dp.toPx()
+                            val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                            val cornerRadius = 8.dp.toPx()
+                            // Draw the dashed border
+                            drawRoundRect(
+                                color = Color.Black,
+                                style = Stroke(width = borderStrokeWidth, pathEffect = pathEffect),
+                                cornerRadius = CornerRadius(cornerRadius)
+                            )
+                        },
+                    shape = RoundedCornerShape(8.dp)
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = "Add Image",
-                        tint = Color.Black,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(24.dp)
+                            .size(100.dp, 161.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Add Image",
+                            tint = Color.Black,
+                        )
+                        Text(
+                            modifier = Modifier.padding(5.dp),
+                            style = MaterialTheme.typography.bodySmall.copy(color = Color.Black),
+                            text = stringResource(id = R.string.pick_photo)
+                        )
+                    }
                 }
             }
         }
@@ -86,7 +111,9 @@ fun ImageSelectorScreen(onAddImageClick: () -> Unit) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ImageSelectorScreen {
+    PhotoCheckerTheme {
+        ImageSelectorScreen {
 
+        }
     }
 }
