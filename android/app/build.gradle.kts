c76d12d6ea32ea39d0a2859f1f9568b635f88947
Jianan <qinxiandiqi@foxmail.com>
2026-06-26 07:55:40 +0800

    chord: 增加版本号

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "cn.qinxiandiqi.photochecker"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
